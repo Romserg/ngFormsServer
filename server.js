00a1fa731519fcecc310d1b0e077e66ac60912d8
Romserg <serg.romankov@gmail.com>
2019-03-04 16:40:30 +0200

    Create server

---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const formidable = require('formidable');
+const util = require('util');
+
+const server = http.createServer(function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type', 'Access-Control-Request-Method', 'Access-Control-Request-Headers');
+
+  if(req.method.toLowerCase() == 'post') {
+    processForm(req, res);
+    return;
+  }
+
+  res.end();
+});
+
+function processForm(req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields) {
+    res.writeHead(200, {
+      'content-type': 'text/plain'
+    });
+
+    res.end(util.inspect({
+      fields: fields
+    }));
+
+    console.log('posted fields:\n');
+    console.log(util.inspect({
+      fields: fields
+    }));
+  });
+}
+
+const port = 3100;
+server.listen(port);
+console.log('server listening on port ' + port);
